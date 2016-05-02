042e42e396254d952309d930799cdeebd3f5f2b7
csmith <csmith@radiantblue.com>
2016-05-02 14:45:45 -0400

    change search to service

--- src/main/java/piazza/services/ingest/controller/Controller.java
+++ src/main/java/piazza/services/ingest/controller/Controller.java
@@ -186,7 +186,7 @@ public class Controller {
 			return new ServiceResponse( objService );
 			
 		} catch (Exception exception) {
-			String message = String.format("Error completing JSON Doc indexing in Elasticsearch from SearchMetadataIngestJob: %s", exception.getMessage());
+			String message = String.format("Error completing JSON Doc indexing in Elasticsearch from ServiceMetadataIngestJob: %s", exception.getMessage());
 			logger.log(message, PiazzaLogger.ERROR);
 			throw new Exception(message);
 		}
