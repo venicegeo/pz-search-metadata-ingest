a341ae2e438a67970741581476004d579147397f
Sonny Saniev <sonny.saniev@rgi-corp.com>
2016-06-13 15:16:55 -0400

    adding venice geo cluster name during startup of elastic instance.

--- config/bootstrap.sh
+++ config/bootstrap.sh
@@ -11,14 +11,18 @@ wget -qO - https://packages.elastic.co/GPG-KEY-elasticsearch | sudo apt-key add 
 echo deb http://packages.elastic.co/elasticsearch/2.x/debian stable main | sudo tee -a /etc/apt/sources.list.d/elasticsearch-2.x.list
 sudo apt-get update && sudo apt-get install elasticsearch
 sudo update-rc.d elasticsearch defaults 95 10
-sudo /etc/init.d/elasticsearch start
 
 #chmod 777 /vagrant/pz-search-metadata-ingest/config/bootstrap.sh
-cp /vagrant/pz-search-metadata-ingest/config/elasticsearch.yml /etc/elasticsearch/elasticsearch.yml
+#cp /vagrant/pz-search-metadata-ingest/config/elasticsearch.yml /etc/elasticsearch/elasticsearch.yml
+
+sudo /etc/init.d/elasticsearch start
 
 # either of the next two lines is needed to be able to access "localhost:9200" from the host os
 sudo echo "network.bind_host: 0" >> /etc/elasticsearch/elasticsearch.yml
 sudo echo "network.host: 0.0.0.0" >> /etc/elasticsearch/elasticsearch.yml
+
+#adding cluster name for venice to yml file
+sudo echo "cluster.name: venice" >> /etc/elasticsearch/elasticsearch.yml
 
 # enable dynamic scripting
 sudo echo "script.inline: on" >> /etc/elasticsearch/elasticsearch.yml
