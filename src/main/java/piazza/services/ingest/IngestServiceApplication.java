d9e03c0992a2817ca7a16f1b9745f31ad268a3af
Patrick D <patrick.doody@rgi-corp.com>
2016-10-06 10:36:56 -0400

    7847 Redirect in secure profile only

--- src/main/java/piazza/services/ingest/IngestServiceApplication.java
+++ src/main/java/piazza/services/ingest/IngestServiceApplication.java
@@ -22,6 +22,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
@@ -42,6 +43,7 @@ public class IngestServiceApplication {
 	}
 	
 	@Configuration
+	@Profile({ "secure" })
 	protected static class SearchConfig extends WebMvcConfigurerAdapter {
 		@Override
 		public void addInterceptors(InterceptorRegistry registry) {
