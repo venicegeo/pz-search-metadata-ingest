a63f90618f6dbfee0a445b41613d80f6be872ce4
Patrick D <patrick.doody@rgi-corp.com>
2016-10-06 09:41:06 -0400

    7847 Add HTTP Interceptor

--- src/main/java/piazza/services/ingest/IngestServiceApplication.java
+++ src/main/java/piazza/services/ingest/IngestServiceApplication.java
@@ -15,9 +15,16 @@
  **/
 package piazza.services.ingest;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
 
 //import piazza.commons.elasticsearch.NativeElasticsearchTemplateConfiguration;
 
@@ -32,5 +39,23 @@ public class IngestServiceApplication {
 	public static void main(String[] args) {
 
 		SpringApplication.run(IngestServiceApplication.class, args); //NOSONAR
+	}
+	
+	@Configuration
+	protected static class GatewayConfig extends WebMvcConfigurerAdapter {
+		@Override
+		public void addInterceptors(InterceptorRegistry registry) {
+			registry.addInterceptor(new HandlerInterceptorAdapter() {
+				@Override
+				public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+					if ((request.getScheme().equals("http")) || (request.getHeader("X-Forwarded-Proto").equals("http"))) {
+						String redirectUrl = String.format("%s://%s%s", "https", request.getServerName(), request.getRequestURI());
+						response.sendRedirect(redirectUrl);
+						return false;
+					}
+					return true;
+				}
+			});
+		}
 	}
 }
