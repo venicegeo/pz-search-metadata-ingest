c4dd5db1df350c10de3bb7a9bb596f28adc44b83
Sonny Saniev <sonny.saniev@rgi-corp.com>
2017-01-17 13:22:34 -0500

    Adding required properties for elastic logging. cleanup.

--- src/main/java/piazza/commons/elasticsearch/NativeElasticsearchTemplate.java
+++ src/main/java/piazza/commons/elasticsearch/NativeElasticsearchTemplate.java
@@ -43,238 +43,148 @@ import model.logger.Severity;
 import util.PiazzaLogger;
 
 //@Component
-public class NativeElasticsearchTemplate
-{
+public class NativeElasticsearchTemplate {
 	private final static Logger LOGGER = LoggerFactory.getLogger(NativeElasticsearchTemplate.class);
 
 	@Autowired
 	private PiazzaLogger logger;
-//	private final Logger log = LoggerFactory.getLogger(
-//		this.getClass());
+	// private final Logger log = LoggerFactory.getLogger(
+	// this.getClass());
 	private Client client;
 	private ObjectMapper mapper;
 
-	public NativeElasticsearchTemplate(){}
-	
-	public NativeElasticsearchTemplate(
-		Client client,
-		ObjectMapper mapper ) {
+	public NativeElasticsearchTemplate() {
+	}
+
+	public NativeElasticsearchTemplate(Client client, ObjectMapper mapper) {
 		this.client = client;
 		this.mapper = mapper;
 	}
 
-	public boolean createIndex(
-			String indexName ) {
-			CreateIndexRequestBuilder createIndexRequestBuilder = client.admin().indices().prepareCreate(
-				indexName);
-			CreateIndexResponse response = createIndexRequestBuilder.execute().actionGet();
+	public boolean createIndex(String indexName) {
+		CreateIndexRequestBuilder createIndexRequestBuilder = client.admin().indices().prepareCreate(indexName);
+		CreateIndexResponse response = createIndexRequestBuilder.execute().actionGet();
 
-			return response.isAcknowledged();
-		}
-	public boolean createIndexWithMapping(
-			String indexName,
-			String type,
-			String mapping) {
-		
-			CreateIndexRequestBuilder createIndexRequestBuilder = client.admin().indices().prepareCreate(
-				indexName);
-	        if (mapping != null) {
-	        	createIndexRequestBuilder.addMapping(type, mapping);
-	        }
-			CreateIndexResponse response = createIndexRequestBuilder.execute().actionGet();
-
-			return response.isAcknowledged();
-		}
-
-	/* CSS presently unused; remove for test code coverage %
-	public boolean createAlias(
-		String indexName,
-		String aliasName ) {
-
-		IndicesAliasesResponse createAliasResponse = client.admin().indices().prepareAliases().addAlias(
-			indexName,
-			aliasName).execute().actionGet();
-
-		return createAliasResponse.isAcknowledged();
-	}
-	*/
-	
-	public boolean indexExists(
-		String indexName ) {
-		return client.admin().indices().prepareExists(
-			indexName).execute().actionGet().isExists();
+		return response.isAcknowledged();
 	}
 
-	public <T extends ESModel> boolean index(
-		String indexName,
-		String type,
-		T o ) {
+	public boolean createIndexWithMapping(String indexName, String type, String mapping) {
+
+		CreateIndexRequestBuilder createIndexRequestBuilder = client.admin().indices().prepareCreate(indexName);
+		if (mapping != null) {
+			createIndexRequestBuilder.addMapping(type, mapping);
+		}
+		CreateIndexResponse response = createIndexRequestBuilder.execute().actionGet();
+
+		return response.isAcknowledged();
+	}
+
+	/*
+	 * CSS presently unused; remove for test code coverage % public boolean
+	 * createAlias( String indexName, String aliasName ) {
+	 * 
+	 * IndicesAliasesResponse createAliasResponse =
+	 * client.admin().indices().prepareAliases().addAlias( indexName,
+	 * aliasName).execute().actionGet();
+	 * 
+	 * return createAliasResponse.isAcknowledged(); }
+	 */
+
+	public boolean indexExists(String indexName) {
+		return client.admin().indices().prepareExists(indexName).execute().actionGet().isExists();
+	}
+
+	public <T extends ESModel> boolean index(String indexName, String type, T o) {
 		boolean created = false;
 
 		try {
-			String source = mapper.writeValueAsString(
-				o);
-			IndexResponse response = client
-				.prepareIndex(
-					indexName,
-					type,
-					o.getId())
-				.setSource(
-					source)
-				.get();
+			String source = mapper.writeValueAsString(o);
+			IndexResponse response = client.prepareIndex(indexName, type, o.getId()).setSource(source).get();
 			created = response.isCreated();
 
 			if (created) {
-				o.setId(
-					response.getId());
+				o.setId(response.getId());
 			}
-		}
-		catch (Exception e) {
+		} catch (Exception e) {
 			LOGGER.error(e.getMessage(), e);
 			logger.log(e.getMessage(), Severity.ERROR);
-//			log.error(
-//				e.getMessage(),
-//				e);
+			// log.error(
+			// e.getMessage(),
+			// e);
 		}
 
 		return created;
 	}
 
-	/* CSS presently unused; remove for test code coverage %
-	public <T extends ESModel> void bulkIndex(
-		String indexName,
-		String type,
-		Collection<T> objects ) {
-		try {
-			BulkRequestBuilder bulkRequest = client.prepareBulk();
+	/*
+	 * CSS presently unused; remove for test code coverage % public <T extends
+	 * ESModel> void bulkIndex( String indexName, String type, Collection<T>
+	 * objects ) { try { BulkRequestBuilder bulkRequest = client.prepareBulk();
+	 * 
+	 * for (T object : objects) { String source = mapper.writeValueAsString(
+	 * object); bulkRequest.add( client.prepareIndex( indexName, type,
+	 * object.getId()).setSource( source)); }
+	 * 
+	 * BulkResponse response = bulkRequest.execute().get();
+	 * 
+	 * // not so friendly way of allowing collections // unfortunately
+	 * collection does not implement get(index) if (objects instanceof List) {
+	 * List<T> l = (List<T>) objects;
+	 * 
+	 * for (BulkItemResponse item : response.getItems()) { if (!item.isFailed())
+	 * { l.get( item.getItemId()).setId( item.getId()); } } }
+	 * 
+	 * } catch (Exception e) { logger.log(e.getMessage(), PiazzaLogger.ERROR); }
+	 * }
+	 */
 
-			for (T object : objects) {
-				String source = mapper.writeValueAsString(
-					object);
-				bulkRequest.add(
-					client.prepareIndex(
-						indexName,
-						type,
-						object.getId()).setSource(
-							source));
-			}
+	/*
+	 * CSS presently unused; remove for test code coverage % public <T extends
+	 * ESModel> List<T> queryForList( SearchRequestBuilder searchQuery, Class<T>
+	 * clazz ) {
+	 * 
+	 * SearchResponse response = searchQuery.setSize(
+	 * Integer.MAX_VALUE).execute().actionGet();
+	 * 
+	 * List<T> results = new ArrayList<T>( response.getHits().getHits().length);
+	 * 
+	 * for (int i = 0; i < response.getHits().getHits().length; ++i) { try {
+	 * SearchHit hit = response.getHits().getHits()[i]; T result =
+	 * mapper.readValue( hit.getSourceAsString(), clazz); result.setId(
+	 * hit.getId()); results.add( result); } catch (Exception e) {
+	 * logger.log(e.getMessage(), PiazzaLogger.ERROR); } }
+	 * 
+	 * return results; }
+	 * 
+	 * public <T extends ESModel> List<T> queryForPage( SearchRequestBuilder
+	 * searchQuery, int pageNumber, int pageSize, Class<T> clazz ) {
+	 * 
+	 * SearchResponse response = searchQuery .setFrom( pageNumber * pageSize)
+	 * .setSize( pageSize) .execute() .actionGet();
+	 * 
+	 * List<T> results = new ArrayList<T>( response.getHits().getHits().length);
+	 * 
+	 * for (int i = 0; i < response.getHits().getHits().length; ++i) { try {
+	 * SearchHit hit = response.getHits().getHits()[i]; T result =
+	 * mapper.readValue( hit.getSourceAsString(), clazz); result.setId(
+	 * hit.getId()); results.add( result); } catch (Exception e) {
+	 * logger.log(e.getMessage(), PiazzaLogger.ERROR); } }
+	 * 
+	 * results.removeAll( Arrays.asList( "", null));
+	 * 
+	 * return results; }
+	 */
 
-			BulkResponse response = bulkRequest.execute().get();
-
-			// not so friendly way of allowing collections
-			// unfortunately collection does not implement get(index)
-			if (objects instanceof List) {
-				List<T> l = (List<T>) objects;
-
-				for (BulkItemResponse item : response.getItems()) {
-					if (!item.isFailed()) {
-						l.get(
-							item.getItemId()).setId(
-								item.getId());
-					}
-				}
-			}
-
-		}
-		catch (Exception e) {
-			logger.log(e.getMessage(), PiazzaLogger.ERROR);
-		}
-	}
-	*/
-	
-	/* CSS presently unused; remove for test code coverage %
-	public <T extends ESModel> List<T> queryForList(
-		SearchRequestBuilder searchQuery,
-		Class<T> clazz ) {
-
-		SearchResponse response = searchQuery.setSize(
-			Integer.MAX_VALUE).execute().actionGet();
-
-		List<T> results = new ArrayList<T>(
-			response.getHits().getHits().length);
-
-		for (int i = 0; i < response.getHits().getHits().length; ++i) {
-			try {
-				SearchHit hit = response.getHits().getHits()[i];
-				T result = mapper.readValue(
-					hit.getSourceAsString(),
-					clazz);
-				result.setId(
-					hit.getId());
-				results.add(
-					result);
-			}
-			catch (Exception e) {
-				logger.log(e.getMessage(), PiazzaLogger.ERROR);
-			}
-		}
-
-		return results;
-	}
-
-	public <T extends ESModel> List<T> queryForPage(
-		SearchRequestBuilder searchQuery,
-		int pageNumber,
-		int pageSize,
-		Class<T> clazz ) {
-
-		SearchResponse response = searchQuery
-			.setFrom(
-				pageNumber * pageSize)
-			.setSize(
-				pageSize)
-			.execute()
-			.actionGet();
-
-		List<T> results = new ArrayList<T>(
-			response.getHits().getHits().length);
-
-		for (int i = 0; i < response.getHits().getHits().length; ++i) {
-			try {
-				SearchHit hit = response.getHits().getHits()[i];
-				T result = mapper.readValue(
-					hit.getSourceAsString(),
-					clazz);
-				result.setId(
-					hit.getId());
-				results.add(
-					result);
-			}
-			catch (Exception e) {
-				logger.log(e.getMessage(), PiazzaLogger.ERROR);
-			}
-		}
-
-		results.removeAll(
-			Arrays.asList(
-				"",
-				null));
-
-		return results;
-	}
-	*/
-	
-	public <T extends ESModel> T findOne(
-		String index,
-		String type,
-		String id,
-		Class<T> clazz ) {
+	public <T extends ESModel> T findOne(String index, String type, String id, Class<T> clazz) {
 
 		T result = null;
 		try {
-			GetResponse response = client.prepareGet(
-				index,
-				type,
-				id).execute().get();
+			GetResponse response = client.prepareGet(index, type, id).execute().get();
 			if (response.isExists()) {
-				result = mapper.readValue(
-					response.getSourceAsBytes(),
-					clazz);
-				result.setId(
-					response.getId());
+				result = mapper.readValue(response.getSourceAsBytes(), clazz);
+				result.setId(response.getId());
 			}
-		}
-		catch (Exception e) {
+		} catch (Exception e) {
 			LOGGER.error(e.getMessage(), e);
 			logger.log(e.getMessage(), Severity.ERROR);
 		}
@@ -282,64 +192,44 @@ public class NativeElasticsearchTemplate
 		return result;
 	}
 
-	/* CSS presently unused; remove for test code coverage %
-	public <T extends ESModel> T queryForOne(
-		SearchRequestBuilder searchQuery,
-		Class<T> clazz ) {
-		SearchResponse response = searchQuery.execute().actionGet();
+	/*
+	 * CSS presently unused; remove for test code coverage % public <T extends
+	 * ESModel> T queryForOne( SearchRequestBuilder searchQuery, Class<T> clazz
+	 * ) { SearchResponse response = searchQuery.execute().actionGet();
+	 * 
+	 * T result = null;
+	 * 
+	 * try { if (response.getHits().getTotalHits() > 0L) { result =
+	 * mapper.readValue( response.getHits().getHits()[0].getSourceAsString(),
+	 * clazz); result.setId(response.getHits().getHits()[0].getId()); } } catch
+	 * (Exception e) { logger.log(e.getMessage(), PiazzaLogger.ERROR); }
+	 * 
+	 * return result; }
+	 */
 
-		T result = null;
+	public long count(SearchRequestBuilder searchQuery) {
 
-		try {
-			if (response.getHits().getTotalHits() > 0L) {
-				result = mapper.readValue(
-					response.getHits().getHits()[0].getSourceAsString(),
-					clazz);
-				result.setId(response.getHits().getHits()[0].getId());
-			}
-		}
-		catch (Exception e) {
-			logger.log(e.getMessage(), PiazzaLogger.ERROR);
-		}
-
-		return result;
-	}
-	*/
-	
-	public long count(
-		SearchRequestBuilder searchQuery ) {
-
-		SearchResponse response = searchQuery.setSearchType(
-			SearchType.COUNT).execute().actionGet();
+		SearchResponse response = searchQuery.setSearchType(SearchType.COUNT).execute().actionGet();
 
 		return response.getHits().getTotalHits();
 	}
 
-/* 2.x changes query builders CSS 5/14/16
+	/*
+	 * 2.x changes query builders CSS 5/14/16 public SearchRequestBuilder
+	 * NativeSearchQueryBuilder() { return new SearchRequestBuilder(
+	 * this.client); }
+	 */
 	public SearchRequestBuilder NativeSearchQueryBuilder() {
-		return new SearchRequestBuilder(
-			this.client);
+		return new SearchRequestBuilder(this.client, null);
 	}
-*/
-	public SearchRequestBuilder NativeSearchQueryBuilder() {
-		return new SearchRequestBuilder(
-			this.client, null);
-	}
-	
-	public <T extends ESModel> boolean delete(
-		String index,
-		String type,
-		T instance ) {
+
+	public <T extends ESModel> boolean delete(String index, String type, T instance) {
 		boolean result = false;
 
 		try {
-			DeleteResponse response = client.prepareDelete(
-				index,
-				type,
-				instance.getId()).execute().get();
+			DeleteResponse response = client.prepareDelete(index, type, instance.getId()).execute().get();
 			result = response.isFound();
-		}
-		catch (Exception e) {
+		} catch (Exception e) {
 			LOGGER.error(e.getMessage(), e);
 			logger.log(e.getMessage(), Severity.ERROR);
 		}
@@ -347,10 +237,7 @@ public class NativeElasticsearchTemplate
 		return result;
 	}
 
-	public <T extends ESModel> int delete(
-		String index,
-		String type,
-		Collection<T> objects ) {
+	public <T extends ESModel> int delete(String index, String type, Collection<T> objects) {
 		int result = 0;
 
 		try {
@@ -360,26 +247,21 @@ public class NativeElasticsearchTemplate
 			BulkRequestBuilder bulkRequest = client.prepareBulk();
 
 			for (T object : objects) {
-				bulkRequest.add(
-					client.prepareDelete(
-						index,
-						type,
-						object.getId()));
+				bulkRequest.add(client.prepareDelete(index, type, object.getId()));
 			}
 
 			BulkResponse response = bulkRequest.execute().get();
 
 			if (response.hasFailures()) {
 				for (BulkItemResponse item : response.getItems()) {
-					if (!item.isFailed()) ++result;
+					if (!item.isFailed())
+						++result;
 				}
-			}
-			else {
+			} else {
 				result = objects.size();
 			}
 
-		}
-		catch (Exception e) {
+		} catch (Exception e) {
 			LOGGER.error(e.getMessage(), e);
 			logger.log(e.getMessage(), Severity.ERROR);
 		}
@@ -387,16 +269,13 @@ public class NativeElasticsearchTemplate
 		return result;
 	}
 
-	public boolean refresh(
-		String index ) {
+	public boolean refresh(String index) {
 		boolean success = false;
 
 		try {
-			RefreshResponse result = client.admin().indices().prepareRefresh(
-				index).execute().get();
+			RefreshResponse result = client.admin().indices().prepareRefresh(index).execute().get();
 			success = (result.getShardFailures().length == 0);
-		}
-		catch (Exception e) {
+		} catch (Exception e) {
 			LOGGER.error(e.getMessage(), e);
 			logger.log(e.getMessage(), Severity.ERROR);
 		}
--- src/main/java/piazza/commons/elasticsearch/NativeElasticsearchTemplateConfiguration.java
+++ src/main/java/piazza/commons/elasticsearch/NativeElasticsearchTemplateConfiguration.java
@@ -20,7 +20,6 @@ import java.net.UnknownHostException;
 
 import org.elasticsearch.client.Client;
 import org.elasticsearch.client.transport.TransportClient;
-//old import org.elasticsearch.common.settings.ImmutableSettings;
 import org.elasticsearch.common.settings.Settings;
 import org.elasticsearch.common.transport.InetSocketTransportAddress;
 import org.springframework.beans.factory.annotation.Value;
@@ -30,56 +29,25 @@ import org.springframework.context.annotation.Configuration;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 
-import util.PiazzaLogger;
-
 @Configuration
 @EnableAutoConfiguration
 public class NativeElasticsearchTemplateConfiguration {
 	@Value("${elasticsearch.clustername}")
 	private String clustername;
-
-	// move to CF VCAP geoint serv. platform
-//	@Value("${elasticsearch.hostname}")
-//	private String hostname;
 	@Value("${vcap.services.pz-elasticsearch.credentials.hostname}")
 	private String cfhostname;
-
-
 	@Value("${elasticsearch.port}")
 	private Integer port;
 
 	@Bean
 	public Client client() throws UnknownHostException {
-//		TransportClient client = TransportClient.builder().build()
-//		        .addTransportAddress(new InetSocketTransportAddress(InetAddress.getByName(hostname), port));
-		
 		Settings settings = Settings.settingsBuilder().put("cluster.name", clustername).build();
-		
 		TransportClient transportClient = TransportClient.builder().settings(settings).build();
-		
-//		transportClient.addTransportAddress(new InetSocketTransportAddress(
-//											InetAddress.getByName(cfhostname), port));
 		transportClient.addTransportAddress(new InetSocketTransportAddress(new InetSocketAddress(cfhostname, port)));
 
 		return transportClient;
 	}
-/*
-	@Bean
-	public ObjectMapper mapper() {
-		return new ObjectMapper();
-	}
-*/	
-	/*
-	 * need local bean "wiring to JobCommons content (PiazzaLogger),
-	 * strategy to scan JC package fails because this project doesn't use other "util" 
-	 * members (e.g. UUIDgen)
-	 */
-	@Bean
-	public PiazzaLogger logger() {
-		return new PiazzaLogger();
-	}
 
-	
 	@Bean
 	public NativeElasticsearchTemplate template(Client client, ObjectMapper mapper) {
 		return new NativeElasticsearchTemplate(client, mapper);
