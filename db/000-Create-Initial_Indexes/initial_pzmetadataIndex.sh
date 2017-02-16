184fe51e9684c3da21149687580b633bd605a2dc
Sonny Saniev <ssaniev@gmail.com>
2017-02-16 14:44:43 -0500

    updated scripts

--- db/000-Create-Initial_Indexes/initial_pzmetadataIndex.sh
+++ db/000-Create-Initial_Indexes/initial_pzmetadataIndex.sh
@@ -2,4 +2,4 @@
 curl -X POST -H "Content-Type: application/json" -H "Cache-Control: no-cache" -d "{\"settings\": {\"number_of_shards\": 1},\"mappings\": {\"$1\": {\"_all\": {\"enabled\": false},\"properties\": {\"locationCenterPoint\": {\"type\": \"geo_point\"},\"boundingArea\": {\"type\": \"geo_shape\"}}}}}" $2
 
 #Alias mapping to index
-curl -X POST -H "Content-Type: application/json" -H "Cache-Control: no-cache" -d "{\"actions\" : [{ \"add\" : { \"index\" : \"$3\", \"alias\" : \"$4\" } }]}" $5/_aliases
+curl -X POST -H "Content-Type: application/json" -H "Cache-Control: no-cache" -d "{\"actions\" : [{ \"add\" : { \"index\" : \"$3\", \"alias\" : \"$4\" } }]}" $5
