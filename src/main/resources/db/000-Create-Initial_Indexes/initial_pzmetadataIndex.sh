b77416d34a1e67abec981369aec853ce28435d6e
Sonny Saniev <ssaniev@gmail.com>
2017-02-23 11:56:22 -0500

    No more setting the number of shards to an index.

--- src/main/resources/db/000-Create-Initial_Indexes/initial_pzmetadataIndex.sh
+++ src/main/resources/db/000-Create-Initial_Indexes/initial_pzmetadataIndex.sh
@@ -1,5 +1,5 @@
 #Initial index creation
-curl -X POST -H "Content-Type: application/json" -H "Cache-Control: no-cache" -d "{\"settings\": {\"number_of_shards\": 1},\"mappings\": {\"$1\": {\"_all\": {\"enabled\": false},\"properties\": {\"locationCenterPoint\": {\"type\": \"geo_point\"},\"boundingArea\": {\"type\": \"geo_shape\"}}}}}" $2
+curl -X POST -H "Content-Type: application/json" -H "Cache-Control: no-cache" -d "{\"mappings\": {\"$1\": {\"_all\": {\"enabled\": false},\"properties\": {\"locationCenterPoint\": {\"type\": \"geo_point\"},\"boundingArea\": {\"type\": \"geo_shape\"}}}}}" $2
 
 #Alias mapping to index
 curl -X POST -H "Content-Type: application/json" -H "Cache-Control: no-cache" -d "{\"actions\" : [{ \"add\" : { \"index\" : \"$3\", \"alias\" : \"$4\" } }]}" $5
